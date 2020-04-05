361e19bc241d107adf03dbecde5cdea059aff6c4
rifky <rifkyadp@gmail.com>
2020-04-05 21:52:11 +0700

    [FIX] socket io

--- src/server.js
+++ src/server.js
@@ -4,16 +4,27 @@ const logEvent = require('./events/myEmitter');
 const loggingListener = require('./events/logging.listener');
 const appMiddleware = require('./middlewares/app-middlewares');
 const appRoutes = require('./routes/index');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
 
 loggingListener();
-const app = express();
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.emit('type:string', 'hallo');
+});
 app.use(appMiddleware);
 app.use(appRoutes);
-const server = http.createServer(app);
-server.on('error', function(e){
-    logEvent.emit('APP_ERROR',{
+server.on('error', function(e) {
+    logEvent.emit('APP_ERROR', {
         logTitle: 'APP_FAILED',
         logMessage: e
     });
 });
+
+
 module.exports = server;
